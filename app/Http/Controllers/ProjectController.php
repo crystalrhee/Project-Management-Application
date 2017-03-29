8331dcaf6843e1cd991fd77d8d961c62b53026f2
Crystal Rhee <rhee.s@husky.neu.edu>
2017-03-29 13:54:16 -0400

    temporary view with fake data

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -14,7 +14,8 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        $projects = Project::latest()->get();
+        return view('projects.index', compact('projects'));
     }
 
     /**
---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Projects</div>
+
+                <div class="panel-body">
+                    @foreach ($projects as $project)
+                        <article>
+                        <h4> {{ $project->client }} </h4>
+                        <div class="body"> {{ $project->description }}</div>
+                        </article>
+
+                        <hr>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
